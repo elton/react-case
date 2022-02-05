50cf129aa9cb411d085b711a69469072a8201288
Elton Zheng <elton.zheng.sh@gmail.com>
2022-02-05 23:40:24 +0800

    Added some comments for container and presentational components.

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 import Joke from './joke';
 
+// 把获取和管理数据这件事和界面渲染这件事分开。做法就是，把获取和管理数据的逻辑放在父组件，也就是聪明组件；
+// 把渲染界面的逻辑放在子组件，也就是傻瓜组件。
+// 这么做的好处，是可以灵活地修改数据状态管理方式，
+// 比如，最初你可能用 Redux 来管理数据，然后你想要修改为用 Mobx，如果按照这种模式分割组件，那么，你需要改的只有聪明组件，傻瓜组件可以保持原状。
 const RandomJoke = () => {
   const [joke, setJoke] = useState(null);
   useEffect(() => {
--- src/components/container/joke.js
+++ src/components/container/joke.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { HiOutlineEmojiHappy } from 'react-icons/hi';
 
-const Joke = ({ value }) => {
+// 如果不使用React.memo，当 RandomJoke 要渲染 Joke 时，即使传入的 props 是一模一样的，
+// Joke 也要走一遍完整的渲染过程，这就显得浪费了。
+// React.memo 利用了 shouldComponentUpdate，如果props一样，就不再渲染了
+const Joke = React.memo(({ value }) => {
   return (
     <div className='flex m-4 items-center bg-teal-100 p-2 rounded-xl text-neutral-600'>
       <HiOutlineEmojiHappy className='w-16 h-16 pr-4' />
       {value || 'loading...'}
     </div>
   );
-};
+});
 
 export default Joke;
